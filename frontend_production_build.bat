706635be86481e99c0b181af8810131d19df685b
Rodion Ugarov <ugarov.r@gmail.com>
2026-02-06 03:12:37 +0100

    fix script

--- frontend_production_build.bat
+++ frontend_production_build.bat
@@ -1,2 +1,2 @@
 cd C:\Users\Rodion\git\transcribe_yt_video\src\frontend
-npm install && npm run build && git add dist
+npm install && npm run build && git add dist && git commit -m "Update frontend production build" && git push origin
